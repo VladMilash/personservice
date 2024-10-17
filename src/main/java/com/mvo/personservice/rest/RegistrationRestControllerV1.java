cb5f7e971e946483598f73d5a13e1bf3b133b631
Vlad_Milas <12milash12@gmail.com>
2024-10-17 15:30:13 +0300

    Added RegistrationRestControllerV1

---
+++ src/main/java/com/mvo/personservice/rest/RegistrationRestControllerV1.java
@@ -0,0 +1,31 @@
+package com.mvo.personservice.rest;
+
+import com.mvo.personservice.entity.User;
+import com.mvo.personservice.mapper.UserMapper;
+import com.mvo.personservice.service.RegistrationUsersService;
+import dto.RegistrationRequestDTO;
+import dto.UserDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("v1/api/registration/")
+public class RegistrationRestControllerV1 {
+    private final RegistrationUsersService registrationUsersService;
+    private final UserMapper userMapper;
+
+    @PostMapping
+    public Mono<ResponseEntity<UserDTO>> registration
+            (@RequestBody RegistrationRequestDTO request) {
+        return registrationUsersService.registrationUser(request)
+                .map(user -> ResponseEntity.status(HttpStatus.OK)
+                        .body(userMapper.map(user)));
+    }
+}
